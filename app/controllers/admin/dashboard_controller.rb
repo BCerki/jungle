7d645e28edb25f6a2864584cdfc853464ec78b53
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-26 16:30:46 +0000

    display counts on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @num_of_products = count_products
+    @num_of_categories = count_categories
+  end
+
+  def count_products
+    Product.count
+  end
+
+  def count_categories
+    Product.select(:category_id).distinct.count
   end
 end
