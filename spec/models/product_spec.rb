fee8b11cf7b8a953727b837293c2b52a0fe5df1e
Brianna Cerkiewicz <briannac@shaw.ca>
2021-06-01 03:05:43 +0000

    debug tests--create instead of new, and create records properly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,28 +4,44 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it "needs price to be valid" do
-      @category = Category.new(:name => "Bikes")
-      @product = Product.new(:name => "Bug", :price => 5, :quantity => 1, :category => @category.id)
+      category = Category.create(:name => "Bikes")
+      @product = category.products.create({
+        name:  'One-speed',
+        quantity: 10,
+        price: 64.99
+      })
       expect(@product.price).to be_present
     end
 
     it "needs quantity to be valid" do
-      @category = Category.new(:name => "Bikes")
-      @product = Product.new(:name => "Bug", :price => 5, :quantity => 1, :category => @category.id)
+      category = Category.create(:name => "Bikes")
+      @product = category.products.create({
+        name:  'One-speed',
+        quantity: 10,
+        price: 64.99
+      })
       expect(@product.quantity).to be_present
     end
 
     it "needs category to be valid" do
-      @category = Category.new(:name => "Bikes")
-      @product = Product.new(:name => "Bug", :price => 5, :quantity => 1, :category => @category.id)
+      category = Category.create(:name => "Bikes")
+      @product = category.products.create({
+        name:  'One-speed',
+        quantity: 10,
+        price: 64.99
+      })
       expect(@product.category).to be_present
     end
 
-    it "error raising" do
-      @category = Category.new(:name => "Bikes")
-      @product = Product.new(:name => "Bug", :price => 5, :quantity => 1, :category => @category.id)
+    it "gives an error message if name is nil" do
+      category = Category.create(:name => "Bikes")
+      @product = category.products.create({
+        name:  nil,
+        quantity: 10,
+        price: 64.99
+      })
       expect(@product.name).to be_nil
-      expect(@product).to raise_error
+      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
     end
 
 
